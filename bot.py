516c2ade7ceef205af8a228b525a59066daae2d0
rodrigoejcm <rodrigo.ejcm@gmail.com>
2018-12-27 19:05:03 +0000

    incluindo telegram bot

--- bot.py
+++ bot.py
@@ -9,21 +9,21 @@ bot = telegram.Bot(token='')
 chat_id = "-352161746"
 
 while True:
-    stream = True 
-    timeline = True 
-    news = True 
+    stream = False 
+    timeline = False 
+    news = False 
 
     msg = "Running Proccess: \n"
     for process in psutil.process_iter():
         if process.name() == 'python': 
             if process.cmdline()[1] == 'stream_db_twitter.py':
-                stream = False                
+                stream = True                
             if process.cmdline()[1] == 'timeline_fetch.py':
-                timeline = False
+                timeline = True
             if process.cmdline()[1] == 'news_paper_fetch.py':
-                news = False
+                news = True
     if (not stream or not timeline or not news ):    
-        msg = "Stream: " + str(stream) + "\n" + "Timeline: " + str(timeline) + "\n" + "News: " + str(news)
+        msg = msg + "Stream: " + str(stream) + "\n" + "Timeline: " + str(timeline) + "\n" + "News: " + str(news)
         bot.send_message(chat_id=chat_id, text=msg)
         time.sleep(600)
     time.sleep(30)
